16e046cf40df390ed945f973c8548268b1ea4df5
ValentinaKirevaa <valentinakireva635@gmail.com>
2026-05-27 03:16:49 +0300

    Update Selo.h

--- Selo.h
+++ Selo.h
@@ -1,35 +1,26 @@
-#ifndef SELO_H    // zashtita ot dvoino vklyuchvane
+#ifndef SELO_H    
 #define SELO_H
 
-#include "NaselenоMyasto.h"   // Selo nasledqva NaselenоMyasto, zatova go vklyuchvame
+#include "NaselenоMyasto.h" 
 
-// -------------------------------------------------------
-//  Klas Selo (Selo)
-//  Nasledqva NaselenоMyasto i dobavya 2 specifichni poleta:
-//    - imaZemedelie : dali ima zemedelska deinost
-//    - imaUchilishte  : dali ima uchilishte
-// -------------------------------------------------------
-class Selo : public NaselenоMyasto {   // "public NaselenоMyasto" = Selo e vid NaselenоMyasto
+class Selo : public NaselenоMyasto {  
 
-private:                    // private = dostapno samo vatre v klasa Selo
-    bool imaZemedelie;        // true = ima zemedelie, false = nema
-    bool imaUchilishte;         // true = ima uchilishte, false = nema
+private:                   
+    bool imaZemedelie;        
+    bool imaUchilishte;        
 
 public:
-    // Konstruktor: priema vsichki danni na NaselenоMyasto + specifichnite za selo
     Selo(string ime, int naselenie, double byudzhet,
             int procentVazrastni, double razstoqnieDoGrad,
             string naiBlizakGrad, int broiLiniiBus,
             bool imaZemedelie, bool imaUchilishte);
 
-    // Getters za specifichnite poleta
-    bool imaZemedelskaDeinos() const;   // vrasha stoinostta na imaZemedelie
-    bool imaUchilishteMetod()  const;   // vrasha stoinostta na imaUchilishte
+    bool imaZemedelskaDeinos() const;  
+    bool imaUchilishteMetod()  const; 
 
-    // override = zamestya metoda na bazoviq klas NaselenоMyasto
-    void   pokazhiInfo()  const override;   // pokazva infoto + specifichnoto za selo
-    string vzemTip()      const override;   // vrasha "Selo"
-    string kamFailString() const override;   // formatira za zapis myasto tip "VILLAGE"
+    void   pokazhiInfo()  const override; 
+    string vzemTip()      const override;   
+    string kamFailString() const override;  
 };
 
 #endif
