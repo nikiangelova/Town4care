e5d700a05724633751870f69f8e48e8f737d2a19
ValentinaKirevaa <valentinakireva635@gmail.com>
2026-05-27 03:28:01 +0300

    Update GradMalak.cpp

--- GradMalak.cpp
+++ GradMalak.cpp
@@ -2,51 +2,38 @@
 #include "GradMalak.h"
 using namespace std;
 
-
-
-//  Izvikva konstruktora na NaselenоMyasto za obshtite danni, sled koeto inicializira specifichnite poleta.
-
 GradMalak::GradMalak(string ime, int naselenie, double byudzhet,
            int procentVazrastni, double razstoqnieDoGrad,
            string naiBlizakGrad, int broiLiniiBus,
            bool imaObshtina, bool imaBolnica)
     : NaselenоMyasto(ime, naselenie, byudzhet, procentVazrastni,
-                 razstoqnieDoGrad, naiBlizakGrad, broiLiniiBus),  // bazov konstruktor
-      imaObshtina(imaObshtina),   // inicializira obshtinska uprava
-      imaBolnica(imaBolnica)            // inicializira bolnica
+                 razstoqnieDoGrad, naiBlizakGrad, broiLiniiBus),
+      imaObshtina(imaObshtina),
+      imaBolnica(imaBolnica)
 {}
 
-// Getters
 bool GradMalak::imaObshtinaMetod() const { return imaObshtina; }
 bool GradMalak::imaBolnicaMetod()     const { return imaBolnica; }
 
-
-//  pokazhiInfo — pokazva obshtoto + specifichnoto za malak grad
-
 void GradMalak::pokazhiInfo() const {
-    NaselenоMyasto::pokazhiInfo();   // obshtata informaciq ot bazoviq klas
+    NaselenоMyasto::pokazhiInfo();
 
-    // specificno za malak grad
     cout << "  --- MALAK GRAD specificno ---" << endl;
     cout << "  Obshtinska uprava: " << (imaObshtina ? "Da" : "Ne") << endl;
     cout << "  Bolnica          : " << (imaBolnica     ? "Da" : "Ne") << endl;
 }
 
-// vrasha tipa kato tekst
 string GradMalak::vzemTip() const { return "Malak grad"; }
 
-// -------------------------------------------------------
-//  kamFailString — formatira za fail myasto tip "TOWN"
-// -------------------------------------------------------
 string GradMalak::kamFailString() const {
-    return "TOWN|"                         +
-           vzemIme()                       + "|" +
+    return "TOWN|"                                 +
+           vzemIme()                               + "|" +
            to_string(vzemNaselenie())      + "|" +
            to_string(vzemByudzhet())          + "|" +
            to_string(vzemProcentVazrastni())  + "|" +
            to_string(vzemRazstoqnie())  + "|" +
-           vzemNaiBlizakGrad()                + "|" +
+           vzemNaiBlizakGrad()                 + "|" +
            to_string(vzemBroiLiniiBus())   + "|" +
-           to_string(imaObshtina)      + "|" +   // bool -> "0" ili "1"
+           to_string(imaObshtina)      + "|" +
            to_string(imaBolnica);
 }
