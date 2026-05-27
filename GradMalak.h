272583701d13641ab238111adbdb2d74ccc4fd9c
ValentinaKirevaa <valentinakireva635@gmail.com>
2026-05-27 03:26:44 +0300

    Update GradMalak.h

--- GradMalak.h
+++ GradMalak.h
@@ -1,34 +1,26 @@
 #ifndef GRAD_MALAK_H    
 #define GRAD_MALAK_H
 
-#include "NaselenоMyasto.h"   // GradMalak nasledqva NaselenоMyasto
+#include "NaselenоMyasto.h"
 
-
-//  Klas GradMalak (Malak grad)
-//  Nasledqva NaselenоMyasto i dobavya 2 specifichni poleta:
-//    - imaObshtina : dali ima obshtinska uprava
-//    - imaBolnica     : dali ima bolnica ili zdraven dom
-
-class GradMalak : public NaselenоMyasto {   // "public NaselenоMyasto" = GradMalak e vid NaselenоMyasto
+class GradMalak : public NaselenоMyasto {
 
 private:
-    bool imaObshtina;   // true = ima obshtinska uprava
-    bool imaBolnica;       // true = ima bolnica / zdraven dom
+    bool imaObshtina;
+    bool imaBolnica;
 
 public:
-    // Konstruktor: vsichki danni na NaselenоMyasto + specifichnite za malak grad
     GradMalak(string ime, int naselenie, double byudzhet,
          int procentVazrastni, double razstoqnieDoGrad,
          string naiBlizakGrad, int broiLiniiBus,
          bool imaObshtina, bool imaBolnica);
 
-    bool imaObshtinaMetod() const;   // vrasha imaObshtina
-    bool imaBolnicaMetod()     const;   // vrasha imaBolnica
+    bool imaObshtinaMetod() const;
+    bool imaBolnicaMetod()     const;
 
-    // override = zamestya metoda na NaselenоMyasto
     void   pokazhiInfo()  const override;
-    string vzemTip()      const override;   // vrasha "Malak grad"
-    string kamFailString() const override;   // formatira myasto tip "TOWN"
+    string vzemTip()      const override;
+    string kamFailString() const override;
 };
 
 #endif
