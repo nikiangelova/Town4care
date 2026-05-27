9ab3a683d754dfc6bdf1f6498f1c9f1ce7654d47
ValentinaKirevaa <valentinakireva635@gmail.com>
2026-05-27 03:20:58 +0300

    Update NaselenоMyasto.h

--- NaselenоMyasto.h
+++ NaselenоMyasto.h
@@ -5,50 +5,41 @@
 #include <string>      
 using namespace std;   
 
-// -------------------------------------------------------
-//  Bazov klas NaselenоMyasto (Naseleno myasto)
-//  Vsichki naseleni mesta spodelqt tezi danni i metodi.
-//  Selo i GradMalak nasledqvat ot nego.
-// -------------------------------------------------------
 class NaselenоMyasto {
 
-protected:                          // protected = dostapno v naslednicite (Selo, GradMaluk)
-    string ime;                    // ime na naselenoto myasto
-    int    naselenie;              
-    double byudzhet;                  // godishen byudzhet v leva
-    int    procentVazrastni;          // procent zhiteli > 65 godini
-    double razstoqnieDoGrad;          // razstoqnie do nai-blizkiq golyam grad (km)
-    string naiBlizakGrad;             // ime na nai-blizkiq golyam grad
-    int    broiLiniiBus;           // tekusht broi avtobusni linii
+protected:                                  
+    string ime;                            
+    int    naselenie;                      
+    double byudzhet;                          
+    int    procentVazrastni;                  
+    double razstoqnieDoGrad;                  
+    string naiBlizakGrad;                     
+    int    broiLiniiBus;                   
 
-public:                             // public = dostupno otvsqkude
-
+public:                                     
 
     NaselenоMyasto(string ime, int naselenie, double byudzhet,
                int procentVazrastni, double razstoqnieDoGrad,
                string naiBlizakGrad, int broiLiniiBus);
 
-    //vrashtat stoinostite na polyata
-    string vzemIme()           const;   // vrasha imeto
-    int    vzemNaselenie()     const;   // vrasha broa zhiteli
-    double vzemByudzhet()         const;   // vrasha byudzheta
-    int    vzemProcentVazrastni() const;   // vrasha % vazrastni
-    double vzemRazstoqnie() const;   // vrasha razstoqnieto
-    string vzemNaiBlizakGrad()    const;   // vrasha imeto na blizkiq grad
-    int    vzemBroiLiniiBus()  const;   // vrasha broa avtobusni linii
+    string vzemIme()           const;   
+    int    vzemNaselenie()     const;   
+    double vzemByudzhet()         const;   
+    int    vzemProcentVazrastni() const;   
+    double vzemRazstoqnie() const;   
+    string vzemNaiBlizakGrad()    const;   
+    int    vzemBroiLiniiBus()  const;   
 
-    // Osnovna logika: analiz na transportnata nuzda
-    bool eRiskovaZona()       const;  // vyarno ako > 40% sa vazrastni
-    int  preporachaniLinii() const;  // izchislqva preporachaniya broi linii
-    bool nuzhdaeSePoveche()  const;  // vyarno ako ima nuzda ot poveche transport
-    void otchetMobilnost() const;  // pechat palen otchet myasto preporacka
+    bool eRiskovaZona()       const;  
+    int  preporachaniLinii() const;  
+    bool nuzhdaeSePoveche()  const;  
+    void otchetMobilnost() const;  
 
-    // Virtualni metodi: zamestvat se v naslednicite
-    virtual void   pokazhiInfo()  const;   // pokazva informaciq za myastoto
-    virtual string vzemTip()      const;   // vrasha tipa ("Selo", "Malak grad")
-    virtual string kamFailString() const;   // formatira dannite za zapis vav fail
+    virtual void   pokazhiInfo()  const;   
+    virtual string vzemTip()      const;   
+    virtual string kamFailString() const;   
 
-    virtual ~NaselenоMyasto() {}   // virtualen destruktor (vazhen pri nasledqvane myasto ukazateli)
+    virtual ~NaselenоMyasto() {}   
 };
 
-#endif   // krai na zashtitata ot dvoino vklyuchvane
+#endif
