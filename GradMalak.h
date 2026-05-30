0af88f8bb6f6eb99d320998b560e39ad5c251b63
kalinailianova-beep <kalinailianova@gmail.com>
2026-05-30 20:48:58 +0300

    Rename GradMalak class to GradMaluk and update includes

--- GradMalak.h
+++ GradMalak.h
@@ -1,26 +1,30 @@
-#ifndef GRAD_MALAK_H    
-#define GRAD_MALAK_H
+#ifndef GRAD_MALUK_H    
+#define GRAD_MALUK_H
 
-#include "NaselenоMyasto.h"
+#include "NaselenoMyasto.h"   
 
-class GradMalak : public NaselenоMyasto {
+#define NOMINMAX
+#include <windows.h>
+
+class GradMaluk: public NaselenoMyasto {   
 
 private:
-    bool imaObshtina;
-    bool imaBolnica;
+    bool imaObshtina;   
+    bool imaBolnica;       
 
 public:
-    GradMalak(string ime, int naselenie, double byudzhet,
-         int procentVazrastni, double razstoqnieDoGrad,
-         string naiBlizakGrad, int broiLiniiBus,
+    
+    GradMaluk(string ime, int naselenie, double byudzhet,
+         int procentVuzrastni, double razstoqnieDoGrad,
+         string naiBlizukGrad, int broiLiniiBus,
          bool imaObshtina, bool imaBolnica);
 
-    bool imaObshtinaMetod() const;
-    bool imaBolnicaMetod()     const;
+    bool imaObshtinaMetod() const;   
+    bool imaBolnicaMetod() const;   
 
-    void   pokazhiInfo()  const override;
-    string vzemTip()      const override;
-    string kamFailString() const override;
+    void pokazhiInfo() const override;
+    string getTip() const override;   
+    string kumFailString() const override;   
 };
 
 #endif
